f96bfce39ce8cf41f9e13e2fcf948cf67d0ab609
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-12-07 17:26:28 +0100

    07-12-2023

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const llistaParaules = [
     "rata",
     "ata"
 ];
+// Recuperar los elementos del DOM que nos interesan.
+const envoltorioGuanyat = document.getElementsByClassName('envoltorio-popup-guanyat');
+const envoltorioPerdut = document.getElementsByClassName('envoltorio-popup-perdut');
 
 let paraulaSecreta = llistaParaules[Math.floor(Math.random() * 3)].toUpperCase();
 let gions;
@@ -53,7 +56,17 @@ lletras.addEventListener("click", ((e) => {
         } else {
             e.target.classList.add("correcte");
         }
-        console.log(e.target.classList);
         aString();
+        let asGuanyat = 0;
+        for(let i = 0; i<paraulaSecretaLletras.length;i++) {
+            if(paraulaSecretaLletras[i] == true) {
+                asGuanyat++;
+            }
+        }
+        if(asGuanyat == paraulaSecretaLletras.length) {
+            envoltorioGuanyat[0].style.display = 'block';
+        } else if(intetns == 0) {
+            envoltorioPerdut[0].style.display = 'block';
+        }
     }
 }));
