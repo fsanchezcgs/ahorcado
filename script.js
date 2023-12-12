403f56a045361dd73d42ab784df3a748ac26b4d9
fsanchezcgs <fsanchez19962@iesjoanramis.org>
2023-12-12 16:03:42 +0100

    08-12-2023

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ const envoltorioPerdut = document.getElementsByClassName('envoltorio-popup-perdu
 let paraulaSecreta = llistaParaules[Math.floor(Math.random() * 3)].toUpperCase();
 let gions;
 let intetns = 7;
+let errors = 0;
+let asGuanyat = 0;
+
+let contador = 0;
+
+let elCrono;
+let miFecha = new Date();
+let cronometroHtml = document.getElementById("cronometro");
+
+miFecha.setHours(0, 0, 0, 0);
+cronometroHtml.innerHTML = "00:00:00";
+
+let elCuentaAtras;
+let miFechaAtras = new Date();
+let cuentaAtrasHtml = document.getElementById("cuentaAtras");
+
+miFechaAtras.setHours(0, 0, 8, 0);
+let segundosAtras = miFechaAtras.getSeconds();
+
+cuentaAtrasHtml.innerHTML = "00:00:08";
 
 paraulaSecreta = paraulaSecreta.split("");
 
@@ -24,10 +44,10 @@ let gionsArrays = [];
 for(let i = 0;i<paraulaSecreta.length;i++) {
     gionsArrays.push("_ ");
 }
+
 aString();
 
 let lletras = document.getElementById("lletras");
-
 
 // FUNCIONS
 
@@ -36,11 +56,84 @@ function aString() {
     let paraulaDiv = document.getElementById("paraula");
     paraulaDiv.innerHTML = "<span>" + gions + "</span>";
     let intetnsSpans = document.getElementById("intents");
-    intetnsSpans.innerText =  intetns;
+    intetnsSpans.innerText = intetns;
+    let errorsComesos = document.getElementById("errorsComesos");
+    errorsComesos.innerText = `Has comes ${errors} errors`;
+}
+
+function comprovarVictoria() {
+    if(asGuanyat == paraulaSecretaLletras.length) {
+        envoltorioGuanyat[0].style.display = 'block';
+        stop();
+    } else if(intetns == 0) {
+        envoltorioPerdut[0].style.display = 'block';
+        stop();
+    }
+}
+
+function crono() {
+    let horas = miFecha.getHours();
+    let minutos = miFecha.getMinutes();
+    let segundos = miFecha.getSeconds();
+
+    segundos += 1;
+
+    if(segundos == 60) {
+        segundos = 0;
+        minutos += 1;
+
+        miFecha.setMinutes(minutos);
+    }
+
+    miFecha.setSeconds(segundos);
+
+    if (horas < 10) { horas = "0" + horas;}
+    if (minutos < 10) { minutos = "0" + minutos;}
+    if (segundos < 10) { segundos = "0" + segundos;}
+
+    cronometroHtml.innerHTML = horas + ":" + minutos + ":" + segundos;
+}
+
+function cuentaAtras() {
+    let horas = miFechaAtras.getHours();
+    let minutos = miFechaAtras.getMinutes();
+
+    segundos -= 1;
+
+    if(segundos == 0) {
+        segundos = 8;
+        intetns--;
+        errors++;
+        aString();
+        comprovarVictoria();
+    }
+
+    miFechaAtras.setSeconds(segundos);
+
+    if (horas < 10) { horas = "0" + horas;}
+    if (minutos < 10) { minutos = "0" + minutos;}
+    if (segundos < 10) { segundos = "0" + segundos;}
+
+    cuentaAtrasHtml.innerHTML = horas + ":" + minutos + ":" + segundos;
+}
+
+function start() {
+    elCrono = setInterval(crono, 2000);
+    elCuentaAtras = setInterval(cuentaAtras, 1000);
+}
+
+function stop() {
+    clearInterval(elCrono);
+    clearInterval(elCuentaAtras);
 }
 
 // EVENTS
 lletras.addEventListener("click", ((e) => {
+    contador++;
+    segundos = 9;
+    if(contador == 1) {
+        start();
+    }
     let paraulaSecretaLletrasCopia = paraulaSecretaLletras.slice();
     if(e.target.classList.contains("lletra")) {
         for(let i = 0; i<paraulaSecreta.length;i++) {
@@ -53,20 +146,17 @@ lletras.addEventListener("click", ((e) => {
         if(JSON.stringify(paraulaSecretaLletras) === JSON.stringify(paraulaSecretaLletrasCopia)) {
             e.target.classList.add("incorrecte");
             intetns--;
+            errors++;
         } else {
             e.target.classList.add("correcte");
         }
         aString();
-        let asGuanyat = 0;
+        asGuanyat = 0;
         for(let i = 0; i<paraulaSecretaLletras.length;i++) {
             if(paraulaSecretaLletras[i] == true) {
                 asGuanyat++;
             }
         }
-        if(asGuanyat == paraulaSecretaLletras.length) {
-            envoltorioGuanyat[0].style.display = 'block';
-        } else if(intetns == 0) {
-            envoltorioPerdut[0].style.display = 'block';
-        }
+        comprovarVictoria()
     }
 }));
